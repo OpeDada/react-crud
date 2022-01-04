936f8c531a7878f46f87d65b6a1ef2aa6d9a9638
OpeDada <opedada@gmail.com>
2022-01-04 22:38:56 +0900

    change names to first and last

--- src/tables/UserTable.js
+++ src/tables/UserTable.js
@@ -4,15 +4,15 @@ const UserTable = () => (
   <table>
     <thead>
       <tr>
-        <th>Name</th>
-        <th>UserName</th>
+        <th>First Name</th>
+        <th>Last Name</th>
         <th>Actions</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td>Name info</td>
-        <td>UserName info</td>
+        <td>Firs Name info</td>
+        <td>Last Name info</td>
         <td>
           <button className="button muted-button">Edit</button>
           <button className="button muted-button">Delete</button>
