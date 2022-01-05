6abede82334839e1db29ac652e4ece494da6b96a
OpeDada <opedada@gmail.com>
2022-01-05 22:42:40 +0900

    create function to handle input change

--- src/forms/AddUserForm.js
+++ src/forms/AddUserForm.js
@@ -4,12 +4,29 @@ const AddUserForm = (props) => {
   const initialFormState = { id: null, firstname: "", lastname: "" };
   const [user, setUser] = useState(initialFormState);
 
+  const handleInputChange = (event) => {
+    console.log(event);
+    const { name, value } = event.target;
+
+    setUser({ ...user, [name]: value });
+  };
+
   return (
     <form>
       <label>First Name</label>
-      <input type="text" name="firstname" value="" />
+      <input
+        type="text"
+        name="firstname"
+        value={user.firstname}
+        onChange={handleInputChange}
+      />
       <label>Last Name</label>
-      <input type="text" name="lastname" value="" />
+      <input
+        type="text"
+        name="lastname"
+        value={user.lastname}
+        onChange={handleInputChange}
+      />
       <button>Add new user</button>
     </form>
   );
